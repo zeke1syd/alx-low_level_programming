a98e1fe28753c348cf4f04e33396705dbdcc5aea
zeke1syd <derrickamaniampong67@gmail.com>
2023-03-22 05:02:47 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
-        fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-                        return (1);
+	 fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+		return (1);
 }
