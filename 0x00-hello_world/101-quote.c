86b923e55f9044f0e3d64f783c78540c08b92f74
zeke1syd <derrickamaniampong67@gmail.com>
2023-03-22 04:40:43 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-#include <unistd.h>
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int main()
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+        fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+                        return (1);
 }
